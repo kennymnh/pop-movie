f7a3b86a4e301e630afd3779fe9d3f61f6dfa1f5
kennymnh <93839333+kennymnh@users.noreply.github.com>
2024-01-18 11:09:05 +0100

    Initial commit

---
+++ Pop'Movie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pop'Movie
+//
+//  Created by SDV Bordeaux on 18/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pop'Movie/Pop_MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pop_MovieApp.swift
+//  Pop'Movie
+//
+//  Created by SDV Bordeaux on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pop_MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
